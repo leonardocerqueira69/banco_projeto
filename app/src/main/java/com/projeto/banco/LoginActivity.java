830c374eb2ee151b9a8277a4cdddb6df53941f64
leonardocerqueira69 <leonardosilvac89@gmail.com>
2023-12-03 07:07:03 -0300

    .

--- app/src/main/java/com/projeto/banco/LoginActivity.java
+++ app/src/main/java/com/projeto/banco/LoginActivity.java
@@ -1,15 +1,30 @@
 package com.projeto.banco;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
     private TextView registro;
+    private EditText text_email, text_senha;
+    private Button bt_entrar;
+    private ProgressBar progress_bar;
+    String[] mensagens = {"Erro ao logar usuário","Preencha todos os campos"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +41,68 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        bt_entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = text_email.getText().toString();
+                String senha = text_senha.getText().toString();
+
+                if (email.isEmpty() || senha.isEmpty()){
+                    Snackbar snackbar = Snackbar.make(v, mensagens[1],Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.RED);
+                    snackbar.show();
+                }else{
+                    autenticarUsuario(v);
+                }
+            }
+        });
+    }
+
+    private void autenticarUsuario(View v){
+        String email = text_email.getText().toString();
+        String senha = text_senha.getText().toString();
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email,senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    progress_bar.setVisibility(View.VISIBLE);
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            telaPrincipal();
+                        }
+                    }, 2000);
+                }else{
+                    String erro;
+
+                    try{
+                        throw task.getException();
+                    }catch (Exception e){
+                        Snackbar snackbar = Snackbar.make(v, mensagens[0],Snackbar.LENGTH_SHORT);
+                        snackbar.setBackgroundTint(Color.WHITE);
+                        snackbar.setTextColor(Color.RED);
+                        snackbar.show();
+                    }
+                }
+            }
+        });
+    }
+
+    private void telaPrincipal(){
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 
     public void iniciarComponents(){
         registro = findViewById(R.id.registro);
+        text_email = findViewById(R.id.text_email);
+        text_senha = findViewById(R.id.text_senha);
+        bt_entrar = findViewById(R.id.bt_entrar);
+        progress_bar = findViewById(R.id.progress_bar);
     }
 }
